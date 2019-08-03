63eb45a6bb417471e44b8461188f7d1764338814
Yurii Lubynets <yurichix1@gmail.com>
2019-08-03 12:23:49 +0300

    Updates

---
+++ precache-manifest.ba6bdd219a3e70ece44c6129c2591e15.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2edfad699a92031c26b865a88bbc30d3",
+    "url": "/user-management-postal/index.html"
+  },
+  {
+    "revision": "c49fb4bebc5e32cfd54e",
+    "url": "/user-management-postal/static/css/main.1963a295.chunk.css"
+  },
+  {
+    "revision": "5a8ecb715ab6843292cb",
+    "url": "/user-management-postal/static/js/2.90e0fd12.chunk.js"
+  },
+  {
+    "revision": "c49fb4bebc5e32cfd54e",
+    "url": "/user-management-postal/static/js/main.5a14a9b0.chunk.js"
+  },
+  {
+    "revision": "924986199a6bc575042f",
+    "url": "/user-management-postal/static/js/runtime~main.d2610d4a.js"
+  }
+]);
